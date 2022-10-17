5bd856c2bb65648780d3fc0b78282552c074d1ea
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2022-10-17 18:51:37 +0600

    2022-10-17

---
+++ lab.jspdf.app/lab.jspdf.app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab.jspdf.app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
